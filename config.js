3aee9918dac310746b38e7603cd1c0bcd7cb9cc3
matiascarrenook <matias.carreno.ok@gmail.com>
2026-09-11 21:32:54 -0300

    Sitio estático llaveros Pokémon (EmulatorJS + llaves por juego)

---
+++ config.js
@@ -0,0 +1,47 @@
+const GAMES = {
+  "rojo-fuego": {
+    nombre: "Pokémon Rojo Fuego",
+    file: "Pokemon - Edicion Rojo Fuego (Spain).gba",
+    color: "#e0201f",
+    detalle: "FireRed · Kanto",
+    token: "bb08e2a180e69d7c827533511f99a7d8",
+    img: "firered.svg",
+    gameID: 101
+  },
+  "verde-hoja": {
+    nombre: "Pokémon Verde Hoja",
+    file: "Pokemon - Edicion Verde Hoja (Spain).gba",
+    color: "#2e9e4f",
+    detalle: "LeafGreen · Kanto",
+    token: "89071c5322e58ae4abe8086a6c6b5aab",
+    img: "leafgreen.svg",
+    gameID: 102
+  },
+  "rubi": {
+    nombre: "Pokémon Rubí",
+    file: "Pokemon - Edicion Rubi (Spain) (Rev 1).gba",
+    color: "#c2185b",
+    detalle: "Ruby · Hoenn",
+    token: "e70e78b6de5ac347138b94b420feeb6f",
+    img: "ruby.png",
+    gameID: 103
+  },
+  "zafiro": {
+    nombre: "Pokémon Zafiro",
+    file: "Pokemon - Edicion Zafiro (Spain).gba",
+    color: "#2860c0",
+    detalle: "Sapphire · Hoenn",
+    token: "07fda86f3594c8baa18cdd22a59ec95e",
+    img: "sapphire.png",
+    gameID: 104
+  },
+  "esmeralda": {
+    nombre: "Pokémon Esmeralda",
+    file: "Pokemon - Edicion Esmeralda (Spain).gba",
+    color: "#0fa573",
+    detalle: "Emerald · Hoenn",
+    token: "26d96693c3bca56cba5bf1d5d6ebbcb1",
+    img: "emerald.svg",
+    gameID: 105
+  }
+};
